f0600f716abe77020036a19a0a01c8eef2af8306
Barbara White <barbara@sardjv.co.uk>
2020-06-05 14:09:53 +0100

    Use FakeDataJob in the seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,3 @@
 # Import some test data for demonstration purposes.
 
-20.times do
-  User.create(
-    first_name: Faker::Name.unique.first_name,
-    last_name: Faker::Name.unique.last_name,
-    email: Faker::Internet.unique.email,
-  )
-end
+FakeDataJob.perform_later(klass: 'User', quantity: 20)
