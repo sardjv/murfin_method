3b4c5a3763356823a0c0e8b8e46f67010a1ace9e
Alex <alexrudall@users.noreply.github.com>
2020-06-09 19:42:43 +0100

    Add an example spec

--- spec/models/time_range_spec.rb
+++ spec/models/time_range_spec.rb
@@ -13,4 +13,33 @@
 describe TimeRange, type: :model do
   it { expect(TimeRange.new).not_to be_valid }
   it { expect(build(:time_range)).to be_valid }
+
+  context 'with a couple of example types' do
+    let(:jp_type) { create(:time_range_type, name: 'Job Plan Period') }
+    let(:rio_type) { create(:time_range_type, name: 'RIO Appointment') }
+
+    context 'with a couple of example ranges' do
+      let(:start_time) { DateTime.now.change({ hour: 9 }) }
+      let(:end_time) { DateTime.now.change({ hour: 17 }) }
+      let!(:job_plan_period) {
+        create(
+          :time_range,
+          time_range_type_id: jp_type.id,
+          start_time: start_time,
+          end_time: end_time
+        )
+      }
+      let!(:rio_appointment) {
+        create(
+          :time_range,
+          time_range_type_id: jp_type.id,
+          start_time: start_time,
+          end_time: end_time + 1.hour
+        )
+      }
+
+      it { expect(job_plan_period).to be_valid }
+      it { expect(rio_appointment).to be_valid }
+    end
+  end
 end
