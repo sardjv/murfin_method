ea3573906da58129e8c709719b69eef37bd4caff
Barbara White <40230316+barbarawuk@users.noreply.github.com>
2020-06-09 11:33:36 +0100

    remove id as automatically generated

--- spec/factories/user_factory.rb
+++ spec/factories/user_factory.rb
@@ -11,7 +11,6 @@
 #
 FactoryBot.define do
   factory :user do
-    id { Faker::Number.unique.number }
     first_name { Faker::Name.unique.first_name }
     last_name  { Faker::Name.unique.last_name }
     email { Faker::Internet.unique.email }
