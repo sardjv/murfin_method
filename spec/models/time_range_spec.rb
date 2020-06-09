e80e7ae70e9559bb1e08a3d712dcd28227aae57a
Alex <alexrudall@gmail.com>
2020-06-09 19:45:35 +0100

    Improve examples to show they represent multiple appointments

--- spec/models/time_range_spec.rb
+++ spec/models/time_range_spec.rb
@@ -15,31 +15,33 @@ describe TimeRange, type: :model do
   it { expect(build(:time_range)).to be_valid }
 
   context 'with a couple of example types' do
-    let(:jp_type) { create(:time_range_type, name: 'Job Plan Period') }
-    let(:rio_type) { create(:time_range_type, name: 'RIO Appointment') }
+    let(:jp_type) { create(:time_range_type, name: 'Job Plan Periods') }
+    let(:rio_type) { create(:time_range_type, name: 'RIO Appointments') }
 
     context 'with a couple of example ranges' do
       let(:start_time) { DateTime.now.change({ hour: 9 }) }
       let(:end_time) { DateTime.now.change({ hour: 17 }) }
-      let!(:job_plan_period) {
+      let!(:job_plan_periods) {
         create(
           :time_range,
           time_range_type_id: jp_type.id,
           start_time: start_time,
-          end_time: end_time
+          end_time: end_time,
+          value: 2
         )
       }
-      let!(:rio_appointment) {
+      let!(:rio_appointments) {
         create(
           :time_range,
           time_range_type_id: jp_type.id,
           start_time: start_time,
-          end_time: end_time + 1.hour
+          end_time: end_time + 1.hour,
+          value: 3
         )
       }
 
-      it { expect(job_plan_period).to be_valid }
-      it { expect(rio_appointment).to be_valid }
+      it { expect(job_plan_periods).to be_valid }
+      it { expect(rio_appointments).to be_valid }
     end
   end
 end
