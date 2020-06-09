6b804c9ac01217d8d64fd26be4c46ecedf216eca
Alex <alexrudall@users.noreply.github.com>
2020-06-09 19:14:04 +0100

    Run Rubocop -a

--- app/models/time_range.rb
+++ app/models/time_range.rb
@@ -11,5 +11,5 @@
 #  updated_at         :datetime         not null
 #
 class TimeRange < ApplicationRecord
-  validates_presence_of :start_time, :end_time, :value, :time_range_type_id
+  validates :start_time, :end_time, :value, :time_range_type_id, presence: true
 end
