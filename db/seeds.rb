68d8762b10632b7e12dfe2809f8dd1e47a66df77
Alex <alexrudall@users.noreply.github.com>
2020-06-10 17:05:59 +0100

    Seed some random data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
 # Import some test data for demonstration purposes.
 
 FakeDataJob.perform_later(klass: 'User', quantity: 20)
+FakeDataJob.perform_later(klass: 'TimeRangeType', quantity: 20)
+FakeDataJob.perform_later(klass: 'TimeRange', quantity: 2000)
