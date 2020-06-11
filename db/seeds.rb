b276f8b7106d9aa0c8b955cb510f818ea529a2e1
Alex <alexrudall@users.noreply.github.com>
2020-06-11 13:23:43 +0100

    Start to add more consistent seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,29 @@
-# Import some test data for demonstration purposes.
+# Create some dummy data for demonstration purposes.
 
-FakeDataJob.perform_later(klass: 'User', quantity: 20)
-FakeDataJob.perform_later(klass: 'TimeRangeType', quantity: 20)
-FakeDataJob.perform_later(klass: 'TimeRange', quantity: 200)
+# Create Users.
+20.times { FactoryBot.create(:user) }
+
+# Create TimeRangeTypes.
+I18n.t('faker.time_range_type.name').each do |name|
+  FactoryBot.create(:time_range_type, name: name)
+end
+
+# For each TimeRangeType,
+# for each User,
+# for each week of the year,
+# create a TimeRange with a random value.
+TimeRangeType.pluck(:id).each do |time_range_type_id|
+  User.pluck(:id).each do |user_id|
+    (1..51).each do |week|
+      FactoryBot.create(
+        :time_range,
+        time_range_type_id: time_range_type_id,
+        user_id: user_id,
+        value: rand(1..100),
+        start_time: DateTime.commercial(2020, week, 1),
+        end_time: DateTime.commercial(2020, week, 1) + 1.week - 1.second
+      )
+    end
+  end
+end
+
