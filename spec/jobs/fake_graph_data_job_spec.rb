dc7252d93e921ef5034c5966c1a1829750a8a716
Alex <alexrudall@users.noreply.github.com>
2020-06-12 11:15:45 +0100

    Add ability to track fake data to an existing plan

--- spec/jobs/fake_graph_data_job_spec.rb
+++ spec/jobs/fake_graph_data_job_spec.rb
@@ -54,4 +54,44 @@ describe FakeGraphDataJob, type: :job do
       expect(TimeRange.first.value).to be_an(Integer)
     end
   end
+
+  context 'with a static plan' do
+    let(:user) { create(:user) }
+    let(:plan) { create(:time_range_type, name: 'plan') }
+    let(:actuals) { create(:time_range_type, name: 'actuals') }
+
+    before do
+      FakeGraphDataJob.perform_now(
+        story: :static,
+        user: user,
+        time_range_type: plan,
+        graph_start_time: DateTime.new(2020).beginning_of_year,
+        graph_end_time: DateTime.new(2020).end_of_year,
+        unit: :week,
+        volatility: 0
+      )
+    end
+
+    context 'with static actuals' do
+      before do
+        FakeGraphDataJob.perform_now(
+          story: :static,
+          user: user,
+          time_range_type: actuals,
+          graph_start_time: DateTime.new(2020).beginning_of_year,
+          graph_end_time: DateTime.new(2020).end_of_year,
+          unit: :week,
+          volatility: 0
+        )
+      end
+
+      it 'tracks closely to the job plan' do
+        differences = plan.time_ranges.map do |plan|
+          (plan.value - actuals.time_ranges.find_by(start_time: plan.start_time).value).abs
+        end
+
+        expect(differences.uniq).to eq([0])
+      end
+    end
+  end
 end
