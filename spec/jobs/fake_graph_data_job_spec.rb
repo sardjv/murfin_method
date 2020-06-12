d95dd42ccfd81a08196f30b56d3d38befe08a737
Alex <alexrudall@users.noreply.github.com>
2020-06-12 10:09:57 +0100

    Add 'static' graph job

---
+++ spec/jobs/fake_graph_data_job_spec.rb
@@ -0,0 +1,23 @@
+describe FakeGraphDataJob, type: :job do
+  describe 'static' do
+    subject(:job) {
+      FakeGraphDataJob.perform_later(
+        story: :static,
+        user: create(:user),
+        time_range_type: create(:time_range_type),
+        graph_start_time: DateTime.new(2020).beginning_of_year,
+        graph_end_time: DateTime.new(2020).end_of_year,
+        unit: :week
+      )
+    }
+
+    before { perform_enqueued_jobs { job } }
+
+    it 'creates records' do
+      expect(User.count).to eq(1)
+      expect(TimeRange.count).to eq(53)
+      expect(TimeRange.first.value).to be_an(Integer)
+      expect(TimeRange.distinct.pluck(:value).count).to eq(1)
+    end
+  end
+end
