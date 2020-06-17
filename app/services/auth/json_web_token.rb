36ebc06797751ee0c2ce6e9d8ac686a915839c27
Barbara White <barbara@sardjv.co.uk>
2020-06-17 07:44:05 +0100

    readme correction and rubobop fix

--- app/services/auth/json_web_token.rb
+++ app/services/auth/json_web_token.rb
@@ -20,13 +20,10 @@ class Auth::JsonWebToken
     jwks_raw = Net::HTTP.get URI("#{ENV['AUTH0_TOKEN_ISSUER']}.well-known/jwks.json")
     jwks_keys = Array(JSON.parse(jwks_raw)['keys'])
     Hash[
-      jwks_keys
-      .map do |k|
+      jwks_keys.map do |k|
         [
           k['kid'],
-          OpenSSL::X509::Certificate.new(
-            Base64.decode64(k['x5c'].first)
-          ).public_key
+          OpenSSL::X509::Certificate.new(Base64.decode64(k['x5c'].first)).public_key
         ]
       end
     ]
