c3edb13029b4d59a66c95472f87f2d0205e10f98
Alex <alexrudall@users.noreply.github.com>
2020-06-25 15:23:14 +0100

    Refactor

--- app/presenters/dashboard_presenter.rb
+++ app/presenters/dashboard_presenter.rb
@@ -9,17 +9,9 @@ class DashboardPresenter
 
   def bar_chart(user_ids:, plan_id:, actual_id:)
     User.find(user_ids).map do |user|
-      if user.time_ranges.count == 0
-        value = nil
-      else
-        plan_total = user.time_ranges.where(time_range_type_id: plan_id).sum(&:value)
-        actual_total = user.time_ranges.where(time_range_type_id: actual_id).sum(&:value)
-        value = ((actual_total.to_f / plan_total) * 100).to_i
-      end
-
       {
         'name': user.name,
-        'value': value
+        'value': bar_chart_value(user: user, plan_id: plan_id, actual_id: actual_id)
       }
     end
   end
@@ -32,5 +24,15 @@ class DashboardPresenter
         actual_id: TimeRangeType.find_by(name: 'RIO Data').id
       )
     }.to_json
+  end
+
+  private
+
+  def bar_chart_value(user:, plan_id:, actual_id:)
+    if user.time_ranges.any?
+      plan_total = user.time_ranges.where(time_range_type_id: plan_id).sum(&:value)
+      actual_total = user.time_ranges.where(time_range_type_id: actual_id).sum(&:value)
+      ((actual_total.to_f / plan_total) * 100).to_i
+    end
   end
 end
