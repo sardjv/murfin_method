eff682b575bd2ab0f9ab238474c0f324db74e923
Alex <alexrudall@users.noreply.github.com>
2020-06-25 13:55:55 +0100

    Use single quotes

--- app/presenters/homepage_presenter.rb
+++ app/presenters/homepage_presenter.rb
@@ -5,17 +5,17 @@ class HomepagePresenter
 
   def bar_chart
     [
-      { "name": 'Skylar Assaqd', "value": '88' },
-      { "name": 'Angel George', "value": '87' },
-      { "name": 'Gretchen Botosh', "value": '82' },
-      { "name": 'Marcus Bator', "value": '79' },
-      { "name": 'Brandon Vetrovs', "value": '72' },
-      { "name": 'Philip Philips', "value": '68' },
-      { "name": 'Jordyn Korsgaard', "value": '64' },
-      { "name": 'Mira Korsgaard', "value": '60' },
-      { "name": 'Ann Herwitz', "value": '53' },
-      { "name": 'Jaylon Dokidis', "value": '53' },
-      { "name": 'Chance Torff', "value": '53' }
+      { 'name': 'Skylar Assaqd', 'value': '88' },
+      { 'name': 'Angel George', 'value': '87' },
+      { 'name': 'Gretchen Botosh', 'value': '82' },
+      { 'name': 'Marcus Bator', 'value': '79' },
+      { 'name': 'Brandon Vetrovs', 'value': '72' },
+      { 'name': 'Philip Philips', 'value': '68' },
+      { 'name': 'Jordyn Korsgaard', 'value': '64' },
+      { 'name': 'Mira Korsgaard', 'value': '60' },
+      { 'name': 'Ann Herwitz', 'value': '53' },
+      { 'name': 'Jaylon Dokidis', 'value': '53' },
+      { 'name': 'Chance Torff', 'value': '53' }
     ]
   end
 
