214804f09b40a789e7ba67758d9d711d560e6ab5
Alex <alexrudall@users.noreply.github.com>
2020-06-25 13:20:39 +0100

    Add homepage presenter

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,3 +1,10 @@
 class PagesController < ApplicationController
-  def home; end
+  def home
+    @presenter = HomepagePresenter.new(params: params)
+
+    respond_to do |format|
+      format.html
+      format.json { render json: @presenter.to_json }
+    end
+  end
 end
---
+++ app/presenters/homepage_presenter.rb
@@ -0,0 +1,27 @@
+class HomepagePresenter
+  def initialize(args)
+    @params = args[:params]
+  end
+
+  def bar_chart
+    [
+      { "name": 'Skylar Assaqd', "value": '88' },
+      { "name": 'Angel George', "value": '87' },
+      { "name": 'Gretchen Botosh', "value": '82' },
+      { "name": 'Marcus Bator', "value": '79' },
+      { "name": 'Brandon Vetrovs', "value": '72' },
+      { "name": 'Philip Philips', "value": '68' },
+      { "name": 'Jordyn Korsgaard', "value": '64' },
+      { "name": 'Mira Korsgaard', "value": '60' },
+      { "name": 'Ann Herwitz', "value": '53' },
+      { "name": 'Jaylon Dokidis', "value": '53' },
+      { "name": 'Chance Torff', "value": '53' }
+    ]
+  end
+
+  def to_json(*_args)
+    {
+      bar_chart: bar_chart
+    }.to_json
+  end
+end
