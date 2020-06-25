bdcd773d06381054ec2fe6034e1d5fe706c7a600
Alex <alexrudall@users.noreply.github.com>
2020-06-25 12:56:44 +0100

    Add rails-ujs and grab data from /current_path.json

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,10 @@
 class DashboardController < ApplicationController
   def show
     @presenter = DashboardPresenter.new(params: params)
+
+    respond_to do |format|
+      format.html
+      format.json { render json: @presenter.graph_data.to_json }
+    end
   end
 end
