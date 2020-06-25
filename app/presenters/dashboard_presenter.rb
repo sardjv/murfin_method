54ab4ab1ebf9e1ebe32a44e1ebc23ee348ffcda2
Alex <alexrudall@users.noreply.github.com>
2020-06-25 15:13:02 +0100

    Get spec passing

--- app/presenters/dashboard_presenter.rb
+++ app/presenters/dashboard_presenter.rb
@@ -9,12 +9,17 @@ class DashboardPresenter
 
   def bar_chart(user_ids:, plan_id:, actual_id:)
     User.find(user_ids).map do |user|
-      plan_total = user.time_ranges.where(time_range_type_id: plan_id).sum(&:value)
-      actual_total = user.time_ranges.where(time_range_type_id: actual_id).sum(&:value)
-      byebug
+      if user.time_ranges.count == 0
+        value = nil
+      else
+        plan_total = user.time_ranges.where(time_range_type_id: plan_id).sum(&:value)
+        actual_total = user.time_ranges.where(time_range_type_id: actual_id).sum(&:value)
+        value = ((actual_total.to_f / plan_total) * 100).to_i
+      end
+
       {
         'name': user.name,
-        'value': actual_total / plan_total
+        'value': value
       }
     end
   end
