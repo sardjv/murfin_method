c1f6e47a72f958dd01139f8ca2893f87c9155261
Alex <alexrudall@users.noreply.github.com>
2020-06-25 12:56:44 +0100

    Add rails-ujs and grab data from /current_path.json

--- app/presenters/dashboard_presenter.rb
+++ app/presenters/dashboard_presenter.rb
@@ -6,4 +6,20 @@ class DashboardPresenter
   def paginated_users
     User.page(@params[:page])
   end
+
+  def graph_data
+    [
+      { "name": "Skylar Assaqd", "value": "88" },
+      { "name": "Angel George", "value": "87" },
+      { "name": "Gretchen Botosh", "value": "82" },
+      { "name": "Marcus Bator", "value": "79" },
+      { "name": "Brandon Vetrovs", "value": "72"},
+      { "name": "Philip Philips", "value": "68" },
+      { "name": "Jordyn Korsgaard", "value": "64" },
+      { "name": "Mira Korsgaard", "value": "60" },
+      { "name": "Ann Herwitz", "value": "53" },
+      { "name": "Jaylon Dokidis", "value": "53" },
+      { "name": "Chance Torff", "value": "53" },
+    ]
+  end
 end
