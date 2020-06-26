ab599f4242de7fcac0b33ce221719ddeb7245790
Alex <alexrudall@users.noreply.github.com>
2020-06-26 11:05:00 +0100

    Add links to nav

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,13 @@
 class DashboardController < ApplicationController
+  def teams
+    @presenter = DashboardPresenter.new(params: params)
+
+    respond_to do |format|
+      format.html
+      format.json { render json: @presenter.to_json }
+    end
+  end
+
   def individuals
     @presenter = DashboardPresenter.new(params: params)
 
