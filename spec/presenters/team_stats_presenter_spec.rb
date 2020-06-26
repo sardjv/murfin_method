90eb3ef37b116e50a7093d7599c80428b9d18539
Alex <alexrudall@users.noreply.github.com>
2020-06-26 16:19:09 +0100

    WIP team_stats_presenter

--- spec/presenters/team_stats_presenter_spec.rb
+++ spec/presenters/team_stats_presenter_spec.rb
@@ -11,15 +11,15 @@ describe TeamStatsPresenter do
   let(:filter_end_date) { Time.zone.today }
 
   context 'when users have no time range values' do
-    describe 'average_planned_per_month' do
+    describe 'average_weekly_planned_per_month' do
       it 'returns 0' do
-        expect(subject.average_planned_per_month).to eq 0
+        expect(subject.average_weekly_planned_per_month).to eq 0
       end
     end
 
-    describe 'average_actual_per_month' do
+    describe 'average_weekly_actual_per_month' do
       it 'returns 0' do
-        expect(subject.average_actual_per_month).to eq 0
+        expect(subject.average_weekly_actual_per_month).to eq 0
       end
     end
 
@@ -30,67 +30,81 @@ describe TeamStatsPresenter do
     end
   end
 
-  # context 'when users hav time range values' do
-  #   let!(:planned_activity) do
-  #     create(:time_range,
-  #            user_id: user.id,
-  #            time_range_type_id: TimeRangeType.plan_type.id,
-  #            start_time: plan_start_time,
-  #            end_time: plan_end_time,
-  #            value: 1500)
-  #   end
-  #   let!(:actual_activity) do
-  #     create(:time_range,
-  #            user_id: user.id,
-  #            time_range_type_id: TimeRangeType.actual_type.id,
-  #            start_time: actual_start_time,
-  #            end_time: actual_end_time,
-  #            value: 630)
-  #   end
-  #   let(:plan_start_time) { 1.year.ago }
-  #   let(:plan_end_time) { Time.zone.now }
-  #   let(:actual_start_time) { 1.year.ago }
-  #   let(:actual_end_time) { Time.zone.now }
+  context 'when users hav time range values' do
+    let!(:planned_activity) do
+      users.each do |user|
+        create(:time_range,
+              user_id: user.id,
+              time_range_type_id: TimeRangeType.plan_type.id,
+              start_time: plan_start_time,
+              end_time: plan_end_time,
+              value: 1500)
+      end
+    end
+    let!(:actual_activity) do
+      users.each do |user|
+        create(:time_range,
+              user_id: user.id,
+              time_range_type_id: TimeRangeType.actual_type.id,
+              start_time: actual_start_time,
+              end_time: actual_end_time,
+              value: 630)
+      end
+    end
 
-  #   context 'within filter time range' do
-  #     describe 'average_weekly_planned' do
-  #       it 'returns avergae weekly planned values for the time range' do
-  #         expect(subject.average_weekly_planned).to eq 29
-  #       end
-  #     end
+    let(:a_year_ago) { Time.zone.local(2019, 6, 26, 14, 31, 12) }
+    let(:now) { Time.zone.local(2020, 6, 26, 14, 31, 12) }
 
-  #     describe 'average_weekly_actual' do
-  #       it 'returns avergae weekly actual values for the time range' do
-  #         expect(subject.average_weekly_actual).to eq 12
-  #       end
-  #     end
+    let(:plan_start_time) { a_year_ago }
+    let(:plan_end_time) { now }
+    let(:actual_start_time) { a_year_ago }
+    let(:actual_end_time) { now }
 
-  #     describe 'percentage_delivered' do
-  #       it 'returns the percentage delievered over the time range' do
-  #         expect(subject.percentage_delivered).to eq 42
-  #       end
-  #     end
-  #   end
+    context 'within filter time range' do
+      describe 'average_weekly_planned_per_month' do
+        it 'returns average weekly planned values for the time range' do
+          expect(subject.average_weekly_planned_per_month).to eq(
+            [
+              {
+                'June': 23
+              }
+            ]
+          )
+        end
+      end
+
+      describe 'average_weekly_actual_per_month' do
+        it 'returns average weekly actual values for the time range' do
+          expect(subject.average_weekly_actual_per_month).to eq 12
+        end
+      end
+
+      describe 'percentage_delivered_per_month' do
+        it 'returns the percentage delievered over the time range' do
+          expect(subject.percentage_delivered_per_month).to eq 42
+        end
+      end
+    end
 
   #   context 'when no filter defaults to last 12 months' do
   #     let(:filter_start_date) {}
   #     let(:filter_end_date) {}
 
-  #     describe 'average_weekly_planned' do
+  #     describe 'average_weekly_planned_per_month' do
   #       it 'returns 0' do
-  #         expect(subject.average_weekly_planned).to eq 29
+  #         expect(subject.average_weekly_planned_per_month).to eq 29
   #       end
   #     end
 
-  #     describe 'average_weekly_actual' do
+  #     describe 'average_weekly_actual_per_month' do
   #       it 'returns 0' do
-  #         expect(subject.average_weekly_actual).to eq 12
+  #         expect(subject.average_weekly_actual_per_month).to eq 12
   #       end
   #     end
 
-  #     describe 'percentage_delivered' do
+  #     describe 'percentage_delivered_per_month' do
   #       it 'returns 0' do
-  #         expect(subject.percentage_delivered).to eq 42
+  #         expect(subject.percentage_delivered_per_month).to eq 42
   #       end
   #     end
   #   end
@@ -99,21 +113,21 @@ describe TeamStatsPresenter do
   #     let(:plan_start_time) { 1.year.ago - 1.day }
   #     let(:plan_end_time) { 1.year.ago - 1.day }
 
-  #     describe 'average_weekly_planned' do
-  #       it 'returns avergae weekly planned values for the time range' do
-  #         expect(subject.average_weekly_planned).to eq 0
+  #     describe 'average_weekly_planned_per_month' do
+  #       it 'returns average weekly planned values for the time range' do
+  #         expect(subject.average_weekly_planned_per_month).to eq 0
   #       end
   #     end
 
-  #     describe 'average_weekly_actual' do
-  #       it 'returns avergae weekly actual values for the time range' do
-  #         expect(subject.average_weekly_actual).to eq 12
+  #     describe 'average_weekly_actual_per_month' do
+  #       it 'returns average weekly actual values for the time range' do
+  #         expect(subject.average_weekly_actual_per_month).to eq 12
   #       end
   #     end
 
-  #     describe 'percentage_delivered' do
+  #     describe 'percentage_delivered_per_month' do
   #       it 'returns the percentage delievered over the time range' do
-  #         expect(subject.percentage_delivered).to eq 0
+  #         expect(subject.percentage_delivered_per_month).to eq 0
   #       end
   #     end
   #   end
@@ -122,23 +136,23 @@ describe TeamStatsPresenter do
   #     let(:actual_start_time) { Time.zone.tomorrow }
   #     let(:actual_end_time) { Time.zone.tomorrow }
 
-  #     describe 'average_weekly_planned' do
-  #       it 'returns avergae weekly planned values for the time range' do
-  #         expect(subject.average_weekly_planned).to eq 29
+  #     describe 'average_weekly_planned_per_month' do
+  #       it 'returns average weekly planned values for the time range' do
+  #         expect(subject.average_weekly_planned_per_month).to eq 29
   #       end
   #     end
 
-  #     describe 'average_weekly_actual' do
-  #       it 'returns avergae weekly actual values for the time range' do
-  #         expect(subject.average_weekly_actual).to eq 0
+  #     describe 'average_weekly_actual_per_month' do
+  #       it 'returns average weekly actual values for the time range' do
+  #         expect(subject.average_weekly_actual_per_month).to eq 0
   #       end
   #     end
 
-  #     describe 'percentage_delivered' do
+  #     describe 'percentage_delivered_per_month' do
   #       it 'returns the percentage delievered over the time range' do
-  #         expect(subject.percentage_delivered).to eq 0
+  #         expect(subject.percentage_delivered_per_month).to eq 0
   #       end
   #     end
   #   end
-  # end
+  end
 end
