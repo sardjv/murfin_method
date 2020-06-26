b9607bcf7243619b418e364188106a918cb15570
Alex <alexrudall@users.noreply.github.com>
2020-06-26 11:28:10 +0100

    Remove unused code

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,11 +1,5 @@
 class DashboardController < ApplicationController
   def teams
-    @presenter = DashboardPresenter.new(params: params)
-
-    respond_to do |format|
-      format.html
-      format.json { render json: @presenter.to_json }
-    end
   end
 
   def individuals
