f8b565b39d4c8e0fc4cbd3a7b6d7734627e16ff9
Alex <alexrudall@users.noreply.github.com>
2020-06-26 12:47:20 +0100

    Import all JS all at once

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,3 +5,10 @@ import 'bootstrap'
 import '../stylesheets/application'
 
 require.context('../images', true)
+
+// Load all JS on every page here, via this single entry point.
+// It's possible to do this better, either conditionally, or with separate packs,
+// but there are lots of gotchas for potentially minimal performance gain.
+// Currently on step 1 of this: https://stackoverflow.com/a/59495659/4741698
+import '../graphs/bar_chart'
+import '../graphs/line_graph'
