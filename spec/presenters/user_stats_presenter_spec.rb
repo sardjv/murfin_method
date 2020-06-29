851cc65d8873d56755b4d58fbd066812a5da2bff
Barbara White <barbara@sardjv.co.uk>
2020-06-29 14:12:03 +0100

    Update to return nil if no planned or actual data for the filter time range.

--- spec/presenters/user_stats_presenter_spec.rb
+++ spec/presenters/user_stats_presenter_spec.rb
@@ -12,20 +12,20 @@ describe UserStatsPresenter do
 
   context 'when user has no time range values' do
     describe 'average_weekly_planned' do
-      it 'returns 0' do
-        expect(subject.average_weekly_planned).to eq 0
+      it 'returns nil' do
+        expect(subject.average_weekly_planned).to eq nil
       end
     end
 
     describe 'average_weekly_actual' do
-      it 'returns 0' do
-        expect(subject.average_weekly_actual).to eq 0
+      it 'returns nil' do
+        expect(subject.average_weekly_actual).to eq nil
       end
     end
 
     describe 'percentage_delivered' do
-      it 'returns 0' do
-        expect(subject.percentage_delivered).to eq 0
+      it 'returns nil' do
+        expect(subject.percentage_delivered).to eq nil
       end
     end
   end
@@ -55,11 +55,28 @@ describe UserStatsPresenter do
     let(:actual_end_time) { Time.zone.now }
 
     describe 'status' do
-      context 'when 0' do
-        let(:plan_value) { 100 }
+      context 'when no planned data' do
+        let!(:planned_activity) { nil }
+        it 'returns Unknown' do
+          expect(subject.status).to eq 'Unknown'
+        end
+      end
+      context 'when planned data with 0 value' do
+        let(:plan_value) { 0 }
+        it 'returns Really Under' do
+          expect(subject.status).to eq 'Really Under'
+        end
+      end
+      context 'when no actual data' do
+        let!(:actual_activity) { nil }
+        it 'returns Unknown' do
+          expect(subject.status).to eq 'Unknown'
+        end
+      end
+      context 'when actual data with 0 value' do
         let(:actual_value) { 0 }
         it 'returns Really Under' do
-          expect(subject.status).to eq 'Unknown'
+          expect(subject.status).to eq 'Really Under'
         end
       end
       context 'when 1 to 49' do
@@ -112,7 +129,7 @@ describe UserStatsPresenter do
       end
     end
 
-    context 'when no filter defaults to last 12 months' do
+    context 'when no filter (it defaults to last 12 months)' do
       let(:args) { { user: user } }
 
       describe 'average_weekly_planned' do
@@ -139,8 +156,8 @@ describe UserStatsPresenter do
       let(:plan_end_time) { 1.year.ago - 1.day }
 
       describe 'average_weekly_planned' do
-        it 'returns average weekly planned values for the time range' do
-          expect(subject.average_weekly_planned).to eq 0
+        it 'returns nil as no planned data' do
+          expect(subject.average_weekly_planned).to eq nil
         end
       end
 
@@ -151,8 +168,8 @@ describe UserStatsPresenter do
       end
 
       describe 'percentage_delivered' do
-        it 'returns the percentage delievered over the time range' do
-          expect(subject.percentage_delivered).to eq 0
+        it 'returns nil as no planned data' do
+          expect(subject.percentage_delivered).to eq nil
         end
       end
     end
@@ -168,14 +185,14 @@ describe UserStatsPresenter do
       end
 
       describe 'average_weekly_actual' do
-        it 'returns average weekly actual values for the time range' do
-          expect(subject.average_weekly_actual).to eq 0
+        it 'returns nil as no actual data' do
+          expect(subject.average_weekly_actual).to eq nil
         end
       end
 
       describe 'percentage_delivered' do
-        it 'returns the percentage delievered over the time range' do
-          expect(subject.percentage_delivered).to eq 0
+        it 'returns nil as no actual data' do
+          expect(subject.percentage_delivered).to eq nil
         end
       end
     end
