2760f2b217297a7050dfe778256b6501640c4a6e
Alex <alexrudall@users.noreply.github.com>
2020-07-01 13:52:21 +0100

    Add DB benchmarking

--- lib/performance.rb
+++ lib/performance.rb
@@ -2,13 +2,13 @@ class Performance
   # Pass a block to get data on its performance:
   # Performance.test { object.method }
   def self.test
-    log 'test'
     time { yield }
-    memory { yield}
+    memory { yield }
+    database { yield }
   end
 
   def self.time
-    log "TIME: #{ Benchmark.realtime { yield }.round(2) } seconds"
+    log "TIME: #{Benchmark.realtime { yield }.round(2)} seconds"
     line_break
   end
 
@@ -28,11 +28,30 @@ class Performance
     line_break
   end
 
+  def self.database
+    log 'DATABASE:'
+
+    report = SqlTracker.track do
+      yield
+    end
+
+    log report.values.map { |query|
+      {
+        sql: query[:sql],
+        count: query[:count],
+        duration: query[:duration],
+        source: query[:source].uniq
+      }
+    }.to_yaml
+
+    line_break
+  end
+
   def self.line_break
     Rails.logger.info '---------------------------------------'
   end
 
-  def self.log string
+  def self.log(string)
     ActiveSupport::Logger.new(STDOUT).info string
   end
 end
