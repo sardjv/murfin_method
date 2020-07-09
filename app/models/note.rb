706251884f26d9fb5be86bcae564e20a15c7fd7b
Alex <alexrudall@users.noreply.github.com>
2020-07-09 15:37:24 +0100

    Annotate Note file

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: notes
+#
+#  id         :bigint           not null, primary key
+#  content    :text(65535)      not null
+#  state      :integer          default("info"), not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Note < ApplicationRecord
   enum state: { info: 0, action: 1, resolved: 2 }
 
