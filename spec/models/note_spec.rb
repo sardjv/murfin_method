0cc7cae417ae3d521df22df67ed119010a484aad
Alex <alexrudall@users.noreply.github.com>
2020-07-09 15:37:24 +0100

    Annotate Note file

--- spec/models/note_spec.rb
+++ spec/models/note_spec.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: notes
+#
+#  id         :bigint           not null, primary key
+#  content    :text(65535)      not null
+#  state      :integer          default("info"), not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 describe Note, type: :model do
   subject { build(:note) }
 
