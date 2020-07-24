34af311e1d80a9313d5f12315f9a86d8e6cc9a6e
Barbara White <barbara@sardjv.co.uk>
2020-07-24 14:38:01 +0100

    GroupType has_many UserGroups

--- app/models/user_group.rb
+++ app/models/user_group.rb
@@ -2,11 +2,14 @@
 #
 # Table name: user_groups
 #
-#  id         :bigint           not null, primary key
-#  name       :string(255)      not null
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
+#  id            :bigint           not null, primary key
+#  name          :string(255)      not null
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#  group_type_id :bigint           not null
 #
 class UserGroup < ApplicationRecord
-  validates :name, presence: true
+  belongs_to :group_type
+
+  validates :name, :group_type_id, presence: true
 end
--- spec/factories/user_group_factory.rb
+++ spec/factories/user_group_factory.rb
@@ -2,13 +2,15 @@
 #
 # Table name: user_groups
 #
-#  id         :bigint           not null, primary key
-#  name       :string(255)      not null
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
+#  id            :bigint           not null, primary key
+#  name          :string(255)      not null
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#  group_type_id :bigint           not null
 #
 FactoryBot.define do
   factory :user_group do
     name { Faker::Commerce.department }
+    group_type_id { GroupType.all.sample.try(:id) || create(:group_type).id }
   end
 end
