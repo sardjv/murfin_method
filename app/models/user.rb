da5cd5989ef223fcf25a05e31c0975d4483b1e68
Alex <alexrudall@users.noreply.github.com>
2020-07-24 10:06:44 +0100

    Get login working

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,4 +23,8 @@ class User < ApplicationRecord
   validates :email, presence: true
   validates :first_name, presence: true
   validates :last_name, presence: true
+
+  def name
+    "#{first_name} #{last_name}"
+  end
 end
