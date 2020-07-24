7ab175c2aa830d6ec6e79bb634f1274063794f28
Alex <alexrudall@users.noreply.github.com>
2020-07-24 09:33:57 +0100

    Don't filter out email with authorize_params

--- config/initializers/auth0.rb
+++ config/initializers/auth0.rb
@@ -4,9 +4,6 @@ Rails.application.config.middleware.use OmniAuth::Builder do
     ENV['AUTH0_CLIENT_ID'],
     ENV['AUTH0_CLIENT_SECRET'],
     'sardjv.eu.auth0.com',
-    callback_path: '/auth/auth0/callback',
-    authorize_params: {
-      scope: 'openid profile email'
-    }
+    callback_path: '/auth/auth0/callback'
   )
 end
