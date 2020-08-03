7338f4cc42a2abf7eed70610c72a13727eef4283
Barbara White <barbara@sardjv.co.uk>
2020-08-03 09:45:35 +0100

    Fixes after switch to enum

--- spec/models/membership_spec.rb
+++ spec/models/membership_spec.rb
@@ -3,7 +3,7 @@
 # Table name: memberships
 #
 #  id            :bigint           not null, primary key
-#  role          :string(255)      default("member"), not null
+#  role          :integer          default("member"), not null
 #  user_group_id :bigint           not null
 #  user_id       :bigint           not null
 #  created_at    :datetime         not null
@@ -17,6 +17,6 @@ describe Membership, type: :model do
   it { expect(subject).to be_valid }
   it { should validate_presence_of(:user_group) }
   it { should validate_presence_of(:user) }
-  it { should validate_inclusion_of(:role).in_array(Membership::ROLES) }
+  it { should validate_presence_of(:role) }
   it { should have_db_index(%i[user_group_id user_id]).unique }
 end
