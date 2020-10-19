56f69cd95cda931b25f44538746c29199763c012
Barbara White <40230316+barbarawuk@users.noreply.github.com>
2020-10-19 13:33:55 +0100

    Show correct subnav e.g. new user should show 'Users' in subnav and some refactoring

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,13 @@
 class ApplicationController < ActionController::Base
   include SecuredWithOauth
   before_action :authenticate_user!
+  before_action :nav_presenter
   protect_from_forgery with: :exception
   helper_method :user_authenticated?, :current_user_name, :current_user_email
+
+  def nav_presenter
+    return unless @current_user
+
+    @nav_presenter ||= NavPresenter.new(params: params, current_user: @current_user)
+  end
 end
