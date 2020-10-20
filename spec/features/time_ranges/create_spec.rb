ac7470b5c7656d5271b102c2cfd0c02d12e11f2b
Alex <alexrudall@users.noreply.github.com>
2020-10-20 19:04:58 +0100

    Add specs for user time_ranges

---
+++ spec/features/time_ranges/create_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+describe 'Admin creates a time_range', type: :feature, js: true do
+  let(:input) { build(:time_range) }
+
+  before do
+    log_in create(:admin)
+    visit time_ranges_path
+    click_link I18n.t('time_range.add')
+    bootstrap_select input.time_range_type.name, from: I18n.t('time_range.labels.time_range_type')
+    fill_in I18n.t('time_range.labels.value'), with: input.value
+    bootstrap_select input.user.name, from: I18n.t('time_range.labels.user')
+  end
+
+  it 'creates time_range' do
+    expect { click_button I18n.t('time_range.save') }.to change { TimeRange.count }.by(1)
+
+    expect(page).to have_content(I18n.t('time_range.notice.successfully.created'))
+  end
+
+  context 'with end before start' do
+    before do
+      bootstrap_select input.start_time.year - 2, from: I18n.t('time_range.labels.end_time')
+    end
+
+    it 'does not save' do
+      expect { click_button I18n.t('time_range.save') }.not_to change(TimeRange, :count)
+
+      expect(page).to have_content(I18n.t('time_range.notice.could_not_be.created'))
+    end
+  end
+end
---
+++ spec/features/time_ranges/index_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+describe 'Time range index', type: :feature, js: true do
+  let!(:time_range) { create(:time_range) }
+  let(:new_name) { 'Hirthe'.freeze }
+
+  before { log_in create(:admin) }
+
+  context 'when a user name is updated' do
+    before do
+      visit time_ranges_path # Set any caches.
+
+      time_range.user.update(last_name: new_name)
+
+      visit time_ranges_path
+    end
+
+    it 'shows the change in the time range index' do
+      expect(page).to have_content(new_name)
+    end
+  end
+end
