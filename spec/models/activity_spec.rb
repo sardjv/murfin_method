170f59a81a5a45ce473ac8b3c564b1fa26fd3df2
Alex <alexrudall@users.noreply.github.com>
2020-10-28 17:19:47 +0000

    Set schedule durations correctly

--- spec/models/activity_spec.rb
+++ spec/models/activity_spec.rb
@@ -25,10 +25,10 @@ describe Activity, type: :model do
     end
 
     context 'when updating all' do
-      it 'sets the attributes based on the plan' do
+      it 'sets the attributes' do
         expect(subject.day).to eq(day)
-        expect(subject.start_time).to eq(subject.plan.start_date.to_time.in_time_zone + 9.hours)
-        expect(subject.end_time).to eq(subject.plan.end_date.to_time.in_time_zone + 13.hours)
+        expect(subject.start_time).to eq(Time.new(1,1,1,9,0))
+        expect(subject.end_time).to eq(Time.new(1,1,1,13,0))
       end
     end
 
@@ -46,6 +46,7 @@ describe Activity, type: :model do
 
     describe 'to_time_ranges' do
       it { expect(subject.to_time_ranges.first).to be_a(TimeRange) }
+      it { expect(subject.to_time_ranges.first.value).to eq(240) } # Minutes in 4 hours.
       it { expect(subject.to_time_ranges.first.user_id).to eq(subject.plan.user_id) }
     end
   end
