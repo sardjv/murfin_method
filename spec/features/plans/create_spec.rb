345b5a1b2417866b573206afa2c63adfce8b1e47
Alex <alexrudall@users.noreply.github.com>
2020-10-28 14:36:35 +0000

    Update specs

--- spec/features/plans/create_spec.rb
+++ spec/features/plans/create_spec.rb
@@ -13,9 +13,6 @@ describe 'User creates a plan', type: :feature, js: true do
   it 'creates plan for the current_user' do
     wait_for_ajax
     click_link I18n.t('activity.add')
-    bootstrap_select 'Tuesday', from: I18n.t('activity.labels.day')
-    bootstrap_select '09', from: I18n.t('activity.labels.start_time')
-    bootstrap_select '13', from: I18n.t('activity.labels.end_time')
 
     expect { click_button I18n.t('plan.save') }.to change { Plan.count }.by(1)
 
