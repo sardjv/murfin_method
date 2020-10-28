6667e5ad63fa7f25636aed793e3341c04f2fb2a4
Alex <alexrudall@users.noreply.github.com>
2020-10-28 10:07:54 +0000

    Run Rubocop -A

--- config/initializers/reload_erb_changes.rb
+++ config/initializers/reload_erb_changes.rb
@@ -1,11 +1,9 @@
 # Fix for this bug: https://github.com/rails/rails/issues/40332
 
-module ActiveSupport
-  class EventedFileUpdateChecker
-    private
+class ActiveSupport::EventedFileUpdateChecker
+  private
 
-    def boot!
-      Listen.to(*@dtw, force_polling: true, &method(:changed)).start
-    end
+  def boot!
+    Listen.to(*@dtw, force_polling: true, &method(:changed)).start
   end
 end
