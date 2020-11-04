0f17916ce6841a892ad8258d98cc36336d4361e6
Alex <alexrudall@users.noreply.github.com>
2020-11-04 14:17:18 +0000

    Add minutes_per_week to schedule_builder

--- app/services/schedule_builder.rb
+++ app/services/schedule_builder.rb
@@ -1,10 +1,11 @@
 class ScheduleBuilder
   # Rules should be an array: [{ type: 'weekly', day: 'monday' }]
-  def self.call(schedule: nil, start_time: nil, end_time: nil, rules: nil)
+  def self.call(schedule: nil, start_time: nil, end_time: nil, rules: nil, minutes_per_week: nil)
     schedule ||= IceCube::Schedule.new
     schedule = set_start_time(schedule: schedule, value: start_time)
     schedule = set_end_time(schedule: schedule, value: end_time)
-    set_rules(schedule: schedule, value: rules)
+    schedule = set_rules(schedule: schedule, value: rules)
+    set_minutes_per_week(schedule: schedule, value: minutes_per_week)
   end
 
   private_class_method def self.set_start_time(schedule:, value:)
@@ -34,5 +35,13 @@ class ScheduleBuilder
     end
 
     schedule
+  end
+
+  private_class_method def self.set_minutes_per_week(schedule:, value:)
+    return schedule unless value
+
+    schedule = set_start_time(schedule: schedule, value: Time.zone.local(1, 1, 1, 9, 0))
+    schedule = set_end_time(schedule: schedule, value: schedule.start_time + value.minutes)
+    set_rules(schedule: schedule, value: [{ type: 'weekly', day: 'monday' }])
   end
 end
--- spec/services/schedule_builder_spec.rb
+++ spec/services/schedule_builder_spec.rb
@@ -21,4 +21,27 @@ describe ScheduleBuilder do
       expect(rule.to_s).to eq('Weekly on Mondays')
     end
   end
+
+  describe 'minutes_per_week' do
+    subject do
+      ScheduleBuilder.call(
+        minutes_per_week: minutes_per_week
+      )
+    end
+
+    context 'when 8 hours' do
+      let(:minutes_per_week) { 8 * 60 }
+
+      it 'builds a schedule' do
+        expect(subject).to be_an(IceCube::Schedule)
+        expect(subject.start_time).to eq(Time.zone.local(1, 1, 1, 9, 0))
+        expect(subject.end_time).to eq(Time.zone.local(1, 1, 1, 17, 0))
+        expect(subject.rrules.count).to eq(1)
+
+        subject.rrules.each do |rule|
+          expect(rule.to_s).to eq('Weekly on Mondays')
+        end
+      end
+    end
+  end
 end
