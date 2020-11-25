f08e79f27589fb155c99990abf92a97b5961a934
Alex <alexrudall@users.noreply.github.com>
2020-11-25 18:09:00 +0000

    Add spec retries override

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -102,7 +102,7 @@ RSpec.configure do |config|
 
   # Run retry only on features.
   config.around :each, :js do |ex|
-    ex.run_with_retry retry: 3
+    ex.run_with_retry retry: ENV['SPEC_RETRIES'] || 3
   end
 
   # Callback to be run between retries
