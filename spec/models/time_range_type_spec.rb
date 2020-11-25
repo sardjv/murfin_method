4a33f8fb22fbf9f240d2326c96db117eb7f86bcf
Alex <alexrudall@users.noreply.github.com>
2020-11-25 18:09:34 +0000

    Update spec

--- spec/models/time_range_type_spec.rb
+++ spec/models/time_range_type_spec.rb
@@ -12,7 +12,7 @@ describe TimeRangeType, type: :model do
 
   it { expect(subject).to be_valid }
   it { should validate_presence_of(:name) }
-  it { should validate_uniqueness_of(:name) }
+  it { should validate_uniqueness_of(:name).ignoring_case_sensitivity }
   it { should have_db_index(:name).unique }
   it { should have_many(:time_ranges).dependent(:destroy) }
 end
