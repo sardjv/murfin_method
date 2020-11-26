ebe514f38491c7b2a007f8d3713c6135c5efaec0
Alex <alexrudall@users.noreply.github.com>
2020-11-26 13:01:32 +0000

    Remove premature link

--- app/presenters/nav_presenter.rb
+++ app/presenters/nav_presenter.rb
@@ -34,10 +34,7 @@ class NavPresenter
             controllers: ['admin/group_types', 'admin/user_groups'] },
           { label: I18n.t('nav.tabs.time_ranges'),
             path: %i[admin time_ranges],
-            controllers: ['admin/time_ranges'] },
-          { label: I18n.t('nav.tabs.tags'),
-            path: %i[admin tags],
-            controllers: ['admin/tags'] }
+            controllers: ['admin/time_ranges'] }
         ],
         enabled: @current_user&.admin?
       }
