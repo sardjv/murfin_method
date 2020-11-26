ba6abb2ed9b89b92bd6e53a4ab13a36c625b795e
Alex <alexrudall@users.noreply.github.com>
2020-11-26 15:37:38 +0000

    Revert incorrect spec

--- spec/features/admin/user_groups/edit_spec.rb
+++ spec/features/admin/user_groups/edit_spec.rb
@@ -1,42 +1,42 @@
 require 'rails_helper'
 
-describe 'Admin edits a tag', type: :feature, js: true do
-  let!(:tag_type) do
-    create(:tag_type, name: 'Band')
+describe 'Admin edits a user group', type: :feature, js: true do
+  let!(:group_type) do
+    create(:group_type, name: 'Band')
   end
   let!(:band1_group) do
-    create(:tag, tag_type: tag_type, name: 'Band 1')
+    create(:user_group, group_type: group_type, name: 'Band 1')
   end
   let!(:band2_group) do
-    create(:tag, tag_type: tag_type, name: 'Band 2')
+    create(:user_group, group_type: group_type, name: 'Band 2')
   end
 
   before { log_in create(:admin) }
 
-  it 'updates tag' do
-    visit admin_tag_types_path
+  it 'updates user group' do
+    visit admin_group_types_path
 
     page.click_on('Bands')
     within('.card-body') do
       first('.bi-pencil').click
     end
-    fill_in I18n.t('tags.labels.name'), with: 'NHS Band 1'
-    click_button I18n.t('tags.save')
+    fill_in I18n.t('user_groups.labels.name'), with: 'NHS Band 1'
+    click_button I18n.t('user_groups.save')
 
     expect(page).to have_content(I18n.t('notice.successfully.updated', model_name: UserGroup.model_name.human))
     expect(band1_group.reload.name).to eq 'NHS Band 1'
   end
 
   context 'when enter non unique name' do
-    it 'does not update tag' do
-      visit admin_tag_types_path
+    it 'does not update user group' do
+      visit admin_group_types_path
 
       page.click_on('Bands')
       within('.card-body') do
         first('.bi-pencil').click
       end
-      fill_in I18n.t('tags.labels.name'), with: 'Band 2'
-      click_button I18n.t('tags.save')
+      fill_in I18n.t('user_groups.labels.name'), with: 'Band 2'
+      click_button I18n.t('user_groups.save')
 
       expect(page).to have_content(I18n.t('notice.could_not_be.updated', model_name: UserGroup.model_name.human))
       expect(band1_group.reload.name).to eq 'Band 1'
