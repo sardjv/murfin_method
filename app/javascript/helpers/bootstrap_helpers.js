a25bddb6df2b4142892882973808d6ea4340b623
Alex <alexrudall@users.noreply.github.com>
2020-12-02 14:50:07 +0000

    Add basic child-select filter

--- app/javascript/helpers/bootstrap_helpers.js
+++ app/javascript/helpers/bootstrap_helpers.js
@@ -1,19 +1,43 @@
 // Initial render.
 window.addEventListener('turbolinks:load', () => {
-  styleSelects()
-  styleDurations()
+  styleSelects();
+  styleDurations();
 });
 
 // After nested form added.
 $(document).on('cocoon:after-insert', () => {
-  styleSelects()
-  styleDurations()
+  styleSelects();
+  styleDurations();
 });
 
 function styleSelects() {
-  $('select').selectpicker({ width: 'fit', liveSearch: true });
+  $('select').selectpicker({ width: 'fit' });
   $('.rails-bootstrap-forms-datetime-select').addClass('form-inline');
   $('.rails-bootstrap-forms-date-select').addClass('form-inline');
+
+  $('select.filter-child-select').each((_index, select) => {
+    filterChildSelect(select);
+  })
+  $('select.filter-child-select').change(function() {
+    filterChildSelect(this);
+  });
+}
+
+function filterChildSelect(select) {
+  const selectedOption = $(select).find('option:selected');
+  const selectedId = selectedOption.attr('data-id');
+  const selectedTypeId = selectedOption.attr('data-tag-type-id');
+  const childOptions = $('option[data-tag-type-parent-id=' + selectedTypeId + ']');
+
+  // Hide all options in the child select.
+  childOptions.prop('disabled', true).hide();
+
+  // Show all tag options which are children of the selected parent.
+  $('option[data-parent-id=' + selectedId + ']').prop('disabled', false);
+  $('option[data-parent-id=' + selectedId + ']').show();
+
+  // Refresh the JS select overlay.
+  $('select.filter-child-select').selectpicker('refresh');
 }
 
 function styleDurations() {
