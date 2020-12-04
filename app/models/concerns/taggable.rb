9e0868683c3aaf3cc3fcbc652b331577b2a58c5b
Alex <alexrudall@users.noreply.github.com>
2020-12-04 11:47:50 +0000

    Refactor

--- app/models/concerns/taggable.rb
+++ app/models/concerns/taggable.rb
@@ -8,9 +8,8 @@ module Taggable
     has_many :tags, through: :tag_associations
 
     def active_tag_associations
-      existing_associations = tag_associations
       TagType.active_for(self.class).sorted.map do |tag_type|
-        existing_associations.find_by(tag_type_id: tag_type.id) || existing_associations.build(tag_type: tag_type)
+        tag_associations.find_by(tag_type_id: tag_type.id) || tag_associations.build(tag_type: tag_type)
       end
     end
   end
