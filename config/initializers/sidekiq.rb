02c78806b2b35c6a09e90c6eac421902249951f0
Alex <alexrudall@users.noreply.github.com>
2020-12-08 12:30:16 +0000

    Fix Rubocop offense

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,8 +1,8 @@
 sidekiq_config = if ENV['REDIS_PASSWORD']
-  { url: "redis://:#{ENV['REDIS_PASSWORD']}@redis:#{ENV['REDIS_PORT']}/0" }
-else
-  { url: ENV['REDIS_URL'] }
-end
+                   { url: "redis://:#{ENV['REDIS_PASSWORD']}@redis:#{ENV['REDIS_PORT']}/0" }
+                 else
+                   { url: ENV['REDIS_URL'] }
+                 end
 
 Sidekiq.configure_server do |config|
   config.redis = sidekiq_config
