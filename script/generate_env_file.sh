9c155d7078246c6b6a43ee73ed17ae8535ed5aa3
Alex <7175262+gritops@users.noreply.github.com>
2021-01-26 18:48:27 +0000

    Improve script

--- script/generate_env_file.sh
+++ script/generate_env_file.sh
@@ -6,13 +6,14 @@
 # Requires bash and openssl.
 # To run:
 # sh ./script/generate_env_file.sh
+EXAMPLE_FILE=.env.example
 ENV_FILE=.env
+REPLACE=YOU_MUST_CHANGE_THIS_PASSWORD
 if [ -f $ENV_FILE ]
 then echo "$ENV_FILE detected, aborting."
 else echo "Generating a fresh $ENV_FILE file..."
-  cp .env.example $ENV_FILE
+  cp $EXAMPLE_FILE $ENV_FILE
 
-  REPLACE=YOU_MUST_CHANGE_THIS_PASSWORD
   for i in `grep $REPLACE $ENV_FILE`; do
     sed -i -e "1s/$REPLACE/$(openssl rand -hex 32)/;t" -e "1,/$REPLACE/s//$(openssl rand -hex 32)/" $ENV_FILE
   done
