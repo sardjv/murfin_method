7847ea2fd24821688da91102949dec5c6b0ca77c
Artur Szwed <artur1979@gmail.com>
2021-02-01 16:37:48 +0100

    auth: fix request spec and add scenario when auth is via form.

--- spec/requests/auth_spec.rb
+++ spec/requests/auth_spec.rb
@@ -1,35 +1,74 @@
 require 'rails_helper'
 
 RSpec.describe 'Auth', type: :request do
-  describe 'GET admin_dashboard_path' do
-    let(:user) do
-      create(:user,
-             first_name: 'John',
-             last_name: 'Smith',
-             email: 'john@example.com')
-    end
-    context 'when authenticated' do
-      before do
-        log_in(user)
-      end
-
-      it 'returns http success' do
-        get admin_dashboard_path
-        expect(response).to have_http_status(:success)
-      end
-
-      it 'sets the user name and email' do
-        get admin_dashboard_path
-        expect(User.last.first_name).to eq('John')
-        expect(User.last.last_name).to eq('Smith')
-        expect(User.last.email).to eq('john@example.com')
+  context 'via oauth2' do
+    around do |example|
+      ClimateControl.modify AUTH_METHOD: 'oauth2' do
+        example.run
       end
     end
 
-    context 'when not authenticated' do
-      it 'redirects to root_path' do
-        get admin_dashboard_path
-        expect(response).to redirect_to(root_path)
+    describe 'GET admin_dashboard_path' do
+      let(:user) do
+        create(:user,
+              first_name: 'John',
+              last_name: 'Smith',
+              email: 'john@example.com')
+      end
+
+      context 'when authenticated' do
+        before do
+          log_in(user)
+        end
+
+        it 'returns http success' do
+          get admin_dashboard_path
+          expect(response).to have_http_status(:success)
+        end
+
+        it 'sets the user name and email' do
+          get admin_dashboard_path
+          expect(User.last.first_name).to eq('John')
+          expect(User.last.last_name).to eq('Smith')
+          expect(User.last.email).to eq('john@example.com')
+        end
+      end
+
+      context 'when not authenticated' do
+        it 'redirects to root_path' do
+          get admin_dashboard_path
+          expect(response).to redirect_to(root_path)
+        end
+      end
+    end
+  end
+
+  context 'via form' do
+    around do |example|
+      ClimateControl.modify AUTH_METHOD: 'form' do
+        example.run
+      end
+    end
+
+    describe 'GET admin_dashboard_path' do
+      let(:user) { create :user }
+
+      context 'when authenticated' do
+        before do
+          log_in(user)
+        end
+
+        it 'returns http success' do
+          get admin_dashboard_path
+          expect(response).to have_http_status(:success)
+        end
+      end
+
+      context 'when not authenticated' do
+        it 'redirects to root_path' do
+          get admin_dashboard_path
+          expect(response).to redirect_to new_user_session_path
+        end
       end
     end
   end
