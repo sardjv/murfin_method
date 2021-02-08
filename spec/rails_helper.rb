9c17ac1928697f465d85508cf995b685622ad698
Barbara White <40230316+barbarawuk@users.noreply.github.com>
2021-02-08 12:33:41 +0000

    Enable --only-failures on rspec

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -55,6 +55,7 @@ shared_context 'frozen in time examples' do
 end
 
 RSpec.configure do |config|
+  config.example_status_persistence_file_path = 'spec/example_status.txt'
   config.include_context 'frozen in time examples', freeze: true
 
   config.include OmniauthMacros
