fb21418892bf40eff1358b899bc58ab20c94503b
Artur Szwed <artur1979@gmail.com>
2021-05-06 16:08:50 +0200

    flash messages channel: comment log messages.

--- app/javascript/channels/flash_messages_channel.js
+++ app/javascript/channels/flash_messages_channel.js
@@ -4,17 +4,17 @@ import consumer from './consumer'
 if($("meta[name='user_logged_in']")) {
   consumer.subscriptions.create("FlashMessagesChannel", {
     connected() {
-      console.log('FlashMessagesChannel | connected')
+      // console.log('FlashMessagesChannel | connected')
       // Called when the subscription is ready for use on the server
     },
 
     disconnected() {
-      console.log('FlashMessagesChannel | disconnected')
+      // console.log('FlashMessagesChannel | disconnected')
       // Called when the subscription has been terminated by the server
     },
 
     received(data) {
-      console.log('FlashMessagesChannel | received', data)
+      // console.log('FlashMessagesChannel | received', data)
       if (data.cableReady) CableReady.perform(data.operations)
       // Called when there's incoming data on the websocket for this channel
     }
