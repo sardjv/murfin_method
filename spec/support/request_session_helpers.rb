b6665b3e414a1c560a3062e211e00e410fc1d37e
Artur Szwed <artur1979@gmail.com>
2021-05-11 16:37:45 +0200

    request helper: default auth is Devise.

--- spec/support/request_session_helpers.rb
+++ spec/support/request_session_helpers.rb
@@ -1,9 +1,9 @@
 module RequestSessionHelpers
   def log_in(user)
-    if ENV['AUTH_METHOD'] == 'form'
-      login_as user, scope: :user # from Devise https://github.com/heartcombo/devise/wiki/How-To:-Test-with-Capybara
-    else
+    if ENV['AUTH_METHOD'] == 'oauth2'
       allow_any_instance_of(ApplicationController).to receive(:session).and_return(user_id: user.id)
+    else
+      login_as user, scope: :user # from Devise https://github.com/heartcombo/devise/wiki/How-To:-Test-with-Capybara
     end
   end
 end
