848a8b2c5734ed82e6f6d5a631299777d8c5e82c
Artur Szwed <artur1979@gmail.com>
2021-05-12 14:21:19 +0200

    spec helpers: revert changes.

--- spec/support/feature_session_helpers.rb
+++ spec/support/feature_session_helpers.rb
@@ -1,12 +1,12 @@
 module FeatureSessionHelpers
   def log_in(user)
-    if ENV['AUTH_METHOD'] == 'oauth2'
+    if ENV['AUTH_METHOD'] == 'form'
+      login_as user, scope: :user # from Devise https://github.com/heartcombo/devise/wiki/How-To:-Test-with-Capybara
+    else
       mock_valid_auth_hash(user)
       Rails.application.env_config['omniauth.auth'] = OmniAuth.config.mock_auth[:auth0]
       Rails.application.env_config['omniauth.params'] = { 'login' => '' }
       visit '/auth/auth0/callback'
-    else
-      login_as user, scope: :user # from Devise https://github.com/heartcombo/devise/wiki/How-To:-Test-with-Capybara
     end
   end
 end
