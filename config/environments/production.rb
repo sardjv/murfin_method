fc41fe3cbde2debf757d0f001b7ca9dfc904b9fa
Artur Szwed <artur1979@gmail.com>
2021-05-19 15:29:16 +0200

    cable: add heroku host to action cable allowed request origins.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,4 +88,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_cable.allowed_request_origins = ['https://murfin-method.herokuapp.com']
 end
