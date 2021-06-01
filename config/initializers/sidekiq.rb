4fd93de4970e616ceae4c113eacb56e1103fbff8
Artur Szwed <artur1979@gmail.com>
2021-06-01 10:59:10 +0200

    rubocop: fix offences.

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,7 @@
 sidekiq_config = if ENV['REDIS_URL'] && !ENV['REDIS_PASSWORD']
-                  { url: "#{ENV['REDIS_URL']}/0" }
+                   { url: "#{ENV['REDIS_URL']}/0" }
                  else
-                  { url: "redis://:#{ENV['REDIS_PASSWORD']}@#{ENV['REDIS_HOST']}:#{ENV['REDIS_PORT']}/0" }
+                   { url: "redis://:#{ENV['REDIS_PASSWORD']}@#{ENV['REDIS_HOST']}:#{ENV['REDIS_PORT']}/0" }
                  end
 
 Sidekiq.configure_server do |config|
