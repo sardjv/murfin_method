6e35e632f1dee59bc83c5566c16b97428995f725
Artur Szwed <artur1979@gmail.com>
2021-06-24 15:25:05 +0200

    team individuals: fix bar chart height for small team.

--- app/helpers/charts_helper.rb
+++ app/helpers/charts_helper.rb
@@ -3,6 +3,14 @@
 module ChartsHelper
   BAR_CHART_BAR_THICKNESS = 10
   BAR_CHART_BAR_MARGINS = 14
+  BAR_CHART_CONTAINER_DEFAULT_HEIGHT = 400
+
+  def bar_chart_scroll_container_height_css(users_count)
+    return 'height: auto' if users_count.try(:zero?)
+    return "height: #{BAR_CHART_CONTAINER_DEFAULT_HEIGHT}px !important" if users_count > 15
+
+    "height: #{((BAR_CHART_BAR_THICKNESS + BAR_CHART_BAR_MARGINS) * users_count) + 20}px !important"
+  end
 
   def bar_chart_container_height_css(users_count)
     return 'height: auto' if users_count.try(:zero?)
