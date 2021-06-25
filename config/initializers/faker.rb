791e10fa4e1328f306fdb2cffe5af8694caef8c0
Artur Szwed <artur1979@gmail.com>
2021-06-25 11:51:21 +0200

    faker: fix initializer.

--- config/initializers/faker.rb
+++ config/initializers/faker.rb
@@ -1,6 +1,9 @@
-class Faker::Murfin::TimeRangeType < Faker::Base
-  def self.name
-    fetch('time_range_type.name')
+module Faker::Murfin # rubocop:disable Style/ClassAndModuleChildren
+  class TimeRangeType < Faker::Base
+    def self.name
+      fetch('time_range_type.name')
+    end
   end
 end
+
 Faker.prepend Faker::Murfin
