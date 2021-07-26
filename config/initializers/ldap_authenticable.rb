c466421781983ddbdd6b16f0e93b16b8ac9aa969
Artur Szwed <artur1979@gmail.com>
2021-07-26 13:52:39 +0200

    circleci: approach to fix specs.

--- config/initializers/ldap_authenticable.rb
+++ config/initializers/ldap_authenticable.rb
@@ -3,7 +3,7 @@ require 'devise/strategies/authenticatable'
 
 class Devise::Strategies::LdapAuthenticatable < Devise::Strategies::Authenticatable
   def valid?
-    ENV['AUTH_METHOD'].split(',').include?('ldap') && params[:ldap_user].present?
+    ENV['AUTH_METHOD'] && ENV['AUTH_METHOD'].split(',').include?('ldap') && params[:ldap_user].present?
   end
 
   def authenticate! # rubocop:disable Metrics/AbcSize, Metrics/MethodLength
